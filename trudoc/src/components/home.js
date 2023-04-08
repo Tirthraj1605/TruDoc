0e75d9928e0de2b166782b54d40ce2bb40720bc1
Anubhav Mendhiratta <anubhavmendhiratta@gmail.com>
2023-04-09 02:43:40 +0530

    minor changes

--- trudoc/src/components/home.js
+++ trudoc/src/components/home.js
@@ -6,12 +6,12 @@ export default function Home() {
       <section className="min-h-screen">
 
         <nav className="py-10 mb-12 flex justify-between dark:text-white">
-          <div className=" h-60 w-60 block mt-4 lg:inline-block text-xl lg:mt-0 dark:text-black  font-bold italic hover:text-black px-4 py-2 rounded  mr-2 text-left fixed"><button onClick={() => { window.scrollTo({ top: 0, behavior: "smooth" }) }}>TD</button></div>
+          <div className=" h-60 w-60 block mt-4 lg:inline-block text-xl lg:mt-0 dark:text-white  font-bold italic hover:text-black px-4 py-2 rounded  mr-2 text-left fixed"><button onClick={() => { window.scrollTo({ top: 0, behavior: "smooth" }) }}>TD</button></div>
           <div className="text-md text-right font-bold text- lg:flex-grow">
 
             <div className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded bg-blue-500 hover:bg-teal-600 mr-2"><button onClick={() => { window.scrollTo({ top: 0, behavior: "smooth" }) }}>Home</button></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <div className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded bg-blue-500 hover:bg-teal-600 mr-2"><button onClick={() => { window.scrollTo({ top: 5000, behavior: "smooth" }) }}>True Prescription</button></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <div className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded bg-blue-500 hover:bg-teal-600 mr-2"><button onClick={() => { window.scrollTo({ top: 850, behavior: "smooth" }) }}>About</button></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <div className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded bg-blue-500 hover:bg-teal-600 mr-2"><button onClick={() => { window.scrollTo({ top: 1010, behavior: "smooth" }) }}>True Prescription</button></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <div className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded bg-blue-500 hover:bg-teal-600 mr-2"><button onClick={() => { window.scrollTo({ top: 1100, behavior: "smooth" }) }}>About</button></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <div className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded bg-blue-500 hover:bg-teal-600 mr-2"><button onClick={() => { window.scrollTo({ top: 5000, behavior: "smooth" }) }}>Contact</button></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           </div>
 
@@ -46,7 +46,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="bg-blue-100">
+        <div className="bg-blue-100 bg-cover rounded-lg">
           <div className=" text-center w-full bg-green-100 p-4 rounded-lg p-10  m-4">
             <span className=" font-bold text-teal-500 text-5xl sm:text-4xl">
               Fill
@@ -59,9 +59,10 @@ export default function Home() {
 
           </div>
 
-          <div className="bg-color-black text-ceter ">
+          <div className="bg-color-black text-center ">
 
-          <br/><br/><br/><form >
+            <br /><br /><br />
+            <form >
 
 
               <label className="text-3xl">
@@ -69,70 +70,88 @@ export default function Home() {
               </label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               <label className="text-3xl">
                 <input type="text" placeholder="Last Name" />
-              </label><br/><br/><br/>
+              </label><br /><br /><br />
               <label className="text-3xl">
                 <input type="date" placeholder="DOB"></input>
-              </label><br/><br/><br/>
+              </label><br /><br /><br />
               <label className="text-3xl">
                 <input type="radio" name="gender" value="M"></input>Male
                 <br></br>
                 <input type="radio" name="gender" value="F"></input>Female
-              </label>
-              <label>
-                <br></br>
+              </label><br /><br /><br />
+              <label className="text-3xl">
                 <input type="email" placeholder="Email" />
-              </label>
-              <label>
-                <input type="submit" value="NEXT"></input>
-              </label>
+              </label><br></br><br></br><br />
+              <label className="text-3xl">
+                <input type="text" placeholder="Family Medical History" />
+              </label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <label className="text-3xl">
+                <input type="text" placeholder="Patient Medical History" />
+              </label><br /><br /><br />
 
-            </form>
-            <form>
-              <label>
-                <select name="symp1">
-                  <option>Fever</option>
-                  <option>Rash</option>
-                  <option>Headache</option>
-                  <option>Weight Gain</option>
-                  <option>Cough</option>
-                  <option>High Fever</option>
-                  <option>Nausea</option>
-                </select>
-                Severity<input type="range" min="0" max="10"></input>
-              </label>
-              <br></br>
-              <label>
-                <select name="symp1">
-                  <option>Fever</option>
-                  <option>Rash</option>
-                  <option>Headache</option>
-                  <option>Weight Gain</option>
-                  <option>Cough</option>
-                  <option>High Fever</option>
-                  <option>Nausea</option>
-                </select>
-                Severity<input type="range" min="0" max="10"></input>
-              </label>
-              <br></br>
-              <label>
-                <select name="symp1">
-                  <option>Fever</option>
-                  <option>Rash</option>
-                  <option>Headache</option>
-                  <option>Weight Gain</option>
-                  <option>Cough</option>
-                  <option>High Fever</option>
-                  <option>Nausea</option>
-                </select>
-                Severity<input type="range" min="0" max="10"></input>
-              </label>
-              <br></br>
-              <input type="Submit" value="Submit"></input>
-              <br></br>
-              <input type="reset" value="Reset"></input>
-            </form>
-            </div>
+            </form><br></br>
+            <div className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded bg-blue-500 hover:bg-teal-600 mr-2"><button onClick={() => { window.scrollTo({ top: 2020, behavior: "smooth" }) }}>NEXT</button></div><br /><br /><br />
+            <br></br><br></br></div>
           </div>
+          <div className="bg-blue-100">
+          <div className=" text-center w-full bg-green-100 p-4 rounded-lg p-10  m-4">
+            <span className=" font-bold text-teal-500 text-5xl sm:text-4xl">
+              Fill
+            </span>&nbsp;&nbsp;
+            <span className=" font-bold text-black font-italic text-5xl sm:text-4xl">in your</span>
+            &nbsp;&nbsp;
+            <span className="font-bold text-blue-900 font-semibold text-5xl sm:text-4xl">
+              Details
+            </span>
+
+          </div>
+          <form>
+            <label className="text-3xl">
+              <select name="symp1">
+                <option>Fever</option>
+                <option>Rash</option>
+                <option>Headache</option>
+                <option>Weight Gain</option>
+                <option>Cough</option>
+                <option>High Fever</option>
+                <option>Nausea</option>
+              </select>&nbsp;&nbsp;<br/><br/>
+              Severity<input type="range" min="0" max="10"></input>
+            </label>
+            <br></br><br></br>
+            <label className="text-3xl">
+              <select name="symp1">
+                <option>Rash</option>
+                <option>Fever</option>
+                <option>Headache</option>
+                <option>Weight Gain</option>
+                <option>Cough</option>
+                <option>High Fever</option>
+                <option>Nausea</option>
+              </select>&nbsp;&nbsp;<br/><br/>
+              Severity<input type="range" min="0" max="10"></input>
+            </label >
+            <br></br><br></br>
+            <label className="text-3xl">
+              <select name="symp1">
+                <option>Headache</option>
+                <option>Rash</option>
+                <option>Fever</option>
+                <option>Weight Gain</option>
+                <option>Cough</option>
+                <option>High Fever</option>
+                <option>Nausea</option>
+              </select>&nbsp;&nbsp;<br/><br/>
+              Severity<input type="range" min="0" max="10"></input>
+            </label>
+            <br></br><br></br>
+            <input type="Submit" value="Submit" className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded bg-blue-500 hover:bg-teal-600 mr-2"></input>
+            <br></br><br></br>
+            <input type="reset" value="Reset" className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded bg-red-500 hover:bg-teal-600 mr-2"></input>
+          </form>
+          </div>
+
+        
       </section>
 
     </main>
